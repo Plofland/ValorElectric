9c6c0053b7013d320ee6191f528e072eabecce3d
Plofland <ploflan@gmail.com>
2026-01-01 01:03:05 -0500

    clear out body content

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,20 +1,14 @@
 import styled from 'styled-components';
-import valorElectricLogo from '/valorElectricLogo.png';
 
 const Body = () => {
-	return (
-		<BodyContainer>
-			<img
-				src={valorElectricLogo}
-				className="logo"
-				alt="Valor Electric logo"
-			/>
-		</BodyContainer>
-	);
+	return <BodyContainer> </BodyContainer>;
 };
 
 export default Body;
 
 const BodyContainer = styled.div`
 	border: 1px solid blue;
+	width: 100%;
+	height: 500px;
+	background-color: #ffffff;
 `;
