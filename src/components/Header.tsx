2b7078a1823c216123963e85f96068cefcc900e7
Plofland <ploflan@gmail.com>
2026-01-01 01:03:45 -0500

    add logo & business name & style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,49 @@
+import styled from 'styled-components';
+// import valorElectricLogo from '/valorElectricLogo.png';
+import transparentValorElectricLogo from '/transparentValorElectricLogo.png';
+
+//todo: download PS so I can edit the logo with a white lightning bolt
+
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<>
+			<HeaderContainer>
+				<HeaderText>
+					<h1>Valor Electric</h1>
+				</HeaderText>
+				<LogoContainer>
+					{/* <ValorElectricLogo
+					src={valorElectricLogo}
+					alt="Valor Electric logo"
+				/> */}
+					<ValorElectricLogo
+						src={transparentValorElectricLogo}
+						alt="Valor Electric logo"
+					/>
+				</LogoContainer>
+			</HeaderContainer>
+		</>
+	);
 };
 
 export default Header;
+
+const HeaderContainer = styled.div`
+	border: 1px solid black;
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+`;
+
+const LogoContainer = styled.div`
+	width: 25%;
+`;
+
+const HeaderText = styled.div`
+	font-size: 3rem;
+`;
+
+const ValorElectricLogo = styled.img`
+	width: 100%;
+	height: 100%;
+`;
